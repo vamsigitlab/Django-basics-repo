ef29a7c5cbb804f8127aa652ab5c6fe49459b68d
vamsigitlab <vamsiparupalli1@gmail.com>
2023-07-17 00:13:30 +0530

    Used some data inside the html page

--- index_page_app/views.py
+++ index_page_app/views.py
@@ -4,7 +4,9 @@ from django.shortcuts import render
 
 def index_page_view(request):
     data = {
+        "show_data": True,
         "name": "Vamsi Parupalli",
-        "course": "Python Django Backend course"
+        "course": "Python Django Backend course",
+        "list": [1,2,3,4,5]
     }
     return render(request, "index.html", context=data)
